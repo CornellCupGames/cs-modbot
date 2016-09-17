f40bb158e63eef0167b54fcaaee3beafe8bb040e
EN-OR-LC0B05-06\CornellCup <CornellCupGames@gmail.com>
2016-09-17 14:16:47 -0400

    added test code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.jade.JadeTemplateEngine;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Hello World");
+
+        get("/hello", (req, res) -> new ModelAndView(map, "test"), new JadeTemplateEngine());
     }
 }
